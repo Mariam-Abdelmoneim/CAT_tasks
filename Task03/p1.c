57d5899eca25f367b35ec6f4a6051256467d6680
Mariam Abdelmoneim <mariammoneim21@gmail.com>
2024-02-15 04:59:02 +0200

    problem 1

--- Task03/p1.c
+++ Task03/p1.c
@@ -3,5 +3,22 @@
 
 int main()
 {
-    
+    int nZeros = 0, nOnes = 0;
+    int num;
+    int size = 8 * sizeof(int);
+
+    printf("Enter any Number: ");
+    scanf("%d", &num);
+
+    for (int i = 0; i < size; i++)
+    {
+        if (num & 1)
+            nOnes++;
+        else
+            nZeros++;
+
+        num>>=1;
+    }
+    printf("Total Zero bits -> %d\n", nZeros);
+    printf("Total One bits -> %d\n", nOnes);
 }
